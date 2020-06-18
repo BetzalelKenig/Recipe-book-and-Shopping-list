7a19f27e4e024b7d705e43360e28bab1a593bdd5
Betzalel Kenig <betzalelkenig@gmail.com>
2020-06-18 13:21:04 +0300

    add all prject files

---
+++ src/app/shoping-list/shoping-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+
+@Component({
+  selector: 'app-shoping-list',
+  templateUrl: './shoping-list.component.html',
+  styleUrls: ['./shoping-list.component.css']
+})
+export class ShopingListComponent implements OnInit {
+  ingredients: Ingredient[];
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit(): void {
+    this.ingredients = this.slService.getIngredients();
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
+}
