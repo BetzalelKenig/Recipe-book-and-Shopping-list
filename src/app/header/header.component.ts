e3a04fd89bcc6ab6e21ab7154f307b0bd09c68a9
Betzalel Kenig <betzalelkenig@gmail.com>
2020-06-30 21:28:10 +0300

    finsh 280

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { DataStorageService } from '../shared/data-storage-service';
+
 
 
 @Component({
@@ -7,5 +9,10 @@ import { Component } from '@angular/core';
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    constructor(private dataStorageService: DataStorageService) {}
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes();
+    }
 
 }
