49ccc2b670422dae09194f65af6b2f7658350cb9
Betzalel Kenig <betzalelkenig@gmail.com>
2020-07-03 13:03:10 +0300

    finish 303

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  //title = '🌭🍔🍗🥣🍰';
- 
+export class AppComponent implements OnInit {
+  // title = '🌭🍔🍗🥣🍰';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
+
 }
